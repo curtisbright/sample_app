85445b2975fd70975f0d97285172180400d8ff9c
Curtis Bright <cbright@uwaterloo.ca>
2012-03-19 18:09:19 -0400

    test the user show page

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ group :development, :test do
   gem 'annotate', '~> 2.4.1.beta'
 end
 
+group :test do
+  gem 'factory_girl_rails', '1.4.0'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,25 +1,14 @@
-#require 'spec_helper'
-#
-#describe "UserPages" do
-#  describe "GET /user_pages" do
-#    it "works! (now write some real specs)" do
-#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get user_pages_index_path
-#      response.status.should be(200)
-#    end
-#  end
-#end
-
 require 'spec_helper'
 
 describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 end
