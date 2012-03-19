a63955ca84d38f3e56a738d8d5aa8938ae7faf1c
Curtis Bright <cbright@uwaterloo.ca>
2012-03-19 19:04:50 -0400

    using form_for

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,6 +12,13 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
   end
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
   describe "signup" do
 
     before { visit signup_path }
