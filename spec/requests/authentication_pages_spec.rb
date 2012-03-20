2dcf7b0ef65d30db085a64379747932b4c2bacfb
Curtis Bright <cbright@uwaterloo.ca>
2012-03-19 21:25:36 -0400

    tests for signin failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,15 @@ describe "Authentication" do
     it { should have_selector('h1',    text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+  end
 end
