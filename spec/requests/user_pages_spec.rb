9e99ac1e99c6e1f8b17be1aa2973b64cf84545c4
Curtis Bright <cbright@uwaterloo.ca>
2012-03-22 22:47:32 -0400

    add some utility functions to help decouple tests from implementation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,68 +1,65 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe 'User pages' do
 
   subject { page }
 
-  describe "profile page" do
+  describe 'profile page' do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should have_heading(user.name) }
+    it { should have_title(user.name) }
   end
 
-  describe "signup page" do
+  describe 'signup page' do
     before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_heading('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 
-  describe "signup" do
+  describe 'signup' do
 
     before { visit signup_path }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button "Create my account" }.not_to change(User, :count)
+    describe 'with invalid information' do
+      it 'should not create a user' do
+        expect { click_button 'Create my account' }.not_to change(User, :count)
       end
 
-      describe "error messages" do
-        before { click_button "Create my account" }
+      describe 'error messages' do
+        before { click_button 'Create my account' }
 
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
-        it { should have_content("Password can't be blank") }
-        it { should have_content("Name can't be blank") }
-        it { should have_content("Email can't be blank") }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
         it { should have_content('Email is invalid') }
         it { should have_content('Password is too short (minimum is 6 characters)') }
-        it { should have_content("Password confirmation can't be blank") }
+        it { should have_content('Password confirmation can\'t be blank') }
       end
     end
 
-    describe "with valid information" do
+    describe 'with valid information' do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        valid_login
       end
 
-      describe "after saving the user" do
-        before { click_button "Create my account" }
+      describe 'after saving the user' do
+        before { click_button 'Create my account' }
         let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_title(user.name) }
+        it { should have_success_message('Welcome') }
 
         it { should have_link('Sign out') }
       end
 
-      it "should create a user" do
+      it 'should create a user' do
         expect do
-          click_button "Create my account"
+          click_button 'Create my account'
         end.to change(User, :count).by(1)
       end
     end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,13 +8,38 @@ def full_title(page_title)
 end
 
 def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
+  fill_in 'Email',    with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Sign in'
+end
+
+def valid_login
+  fill_in 'Name',         with: 'Example User'
+  fill_in 'Email',        with: 'user@example.com' 
+  fill_in 'Password',     with: 'foobar'
+  fill_in 'Confirmation', with: 'foobar'
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text: title)
+  end
+end
+
+RSpec::Matchers.define :have_heading do |heading|
+  match do |page|
+    page.should have_selector('h1', text: heading)
   end
 end
