bbab7d590249ee601f34753f2bdd0c4aca3e421e
Curtis Bright <cbright@uwaterloo.ca>
2012-03-24 08:12:51 -0400

    test friently forwarding for multiple sign in attempts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -87,17 +87,32 @@ describe 'Authentication' do
 
     describe 'for non-signed-in users' do
       let(:user) { FactoryGirl.create(:user) }
-
-      describe 'when attempting to visit a protected page' do
+  
+      describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          valid_signin(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
         end
 
-        describe 'after signing in' do
+        describe "after signing in" do
 
-          it 'should render the desired protected page' do
+          it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
           end
         end
       end
