eb03e38c55fe0d946fa83ae140100a86a313566f
Curtis Bright <cbright@uwaterloo.ca>
2012-03-24 06:22:17 -0400

    check profile and settings only appear when user logged in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,8 @@ describe 'Authentication' do
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
       describe 'after visiting another page' do
         before { click_link 'Home' }
         it { should_not have_error_message('') }
