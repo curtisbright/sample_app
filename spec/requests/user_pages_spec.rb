14d4653e0c1175e8a760965067a9dd5a367979b0
Curtis Bright <cbright@uwaterloo.ca>
2012-03-25 08:42:36 -0400

    tests for stats on profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -238,6 +238,18 @@ describe 'User pages' do
         end
       end
     end
+
+    describe "follower/following counts" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        other_user.follow!(user)
+        visit user_path(user)
+      end
+
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 follower",  href: followers_user_path(user)) }
+    end
+
   end
 
   describe 'following/followers' do
